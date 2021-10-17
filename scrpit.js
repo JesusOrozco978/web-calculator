efa68d9efc2f84f73d1a1bdeec2e5795613a4784
Jesus Orozco <jesusorozco@jesuss-mbp.myfiosgateway.com>
2021-10-17 14:26:10 -0400

    upddate

---
+++ scrpit.js
@@ -0,0 +1,8 @@
+//Take user input
+function sum() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    // add two numbers 
+    var sum = Number(num1) + Number(num2); 
+    document.getElementById("answer").value = sum;
+}
